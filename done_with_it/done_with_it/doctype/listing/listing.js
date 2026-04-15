d031c1ded39954250a839b44d1457a89075477ba
abdullahnatiq1 <abdullahnatiq8765@mail.com>
2026-04-15 16:03:01 +0500

    Routes

--- done_with_it/done_with_it/doctype/listing/listing.js
+++ done_with_it/done_with_it/doctype/listing/listing.js
@@ -1,8 +1,42 @@
 // Copyright (c) 2026, me and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("listing", {
-// 	refresh(frm) {
+frappe.ui.form.on("listing", {
+    refresh: function(frm) {
+        frm.add_custom_button(__('Sum interests'), function() {
+            
+            frappe.call({
+            method: "done_with_it.done_with_it.doctype.listing.listing.getSum",
+            args:{
+                'price': frm.doc.price,
+                "fullName": "Abdullah Natiq"
+            },
+            callback: function(r){
+                console.log(r.message)
+            }
+        
+            });
 
-// 	},
-// });
+       }, __("Actions"));
+
+    //    frm.add_custom_button(__('Something Else'), function() {
+    //         frappe.msgprint("Something Else");
+    //    }, __("Actions"));
+	},
+
+    validate: function(frm){
+        let offers = frm.doc.shown_interests;
+
+        let total_offers_value = 0;
+
+        offers.forEach(row => {
+            total_offers_value += row.offer_price
+        })
+
+        frm.set_value("totals", total_offers_value);
+        frm.refresh();
+
+    }
+});
+
+
--- done_with_it/done_with_it/doctype/listing/listing.py
+++ done_with_it/done_with_it/doctype/listing/listing.py
@@ -1,9 +1,17 @@
 # Copyright (c) 2026, me and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.website.website_generator import WebsiteGenerator
 
 
 class listing(WebsiteGenerator):
 	pass
+
+@frappe.whitelist()
+def getSum(price, fullName):
+
+	print(f"\n\n\n{price}\n\n\n")
+	print(f"\n\n\n{fullName}\n\n\n")
+
+	return price
